1e8e7289e1237c8f3eaaf908902dfb4818ab4a20
Aryan apte <b20186@students.iitmandi.ac.in>
2022-01-22 12:39:43 +0530

    2048 Game

---
+++ Game/Game2048/colors.py
@@ -0,0 +1,36 @@
+GRID_COLOR = "#000000"
+EMPTY_CELL_COLOR = "#f2f2f2"
+SCORE_LABEL_FONT = ("Verdana", 30)
+SCORE_FONT = ("Verdana", 30, "bold")
+GAME_OVER_FONT = ("Verdana", 40, "bold")
+GAME_OVER_FONT_COLOR = "#ffffff"
+WINNER_BG = "#00ff00"
+LOSER_BG = "#ff0000"
+
+CELL_COLORS = {
+    2: "#0066ff",
+    4: "#00ccff",
+    8: "#00ff00",
+    16:"#cc33ff",
+    32:"#ff33cc",
+    64:"#6600ff",
+    128:"#ff6600",
+    256:"#cc6600",
+    512:"#660066",
+    1024:"#99ff66",
+    2048:"#ffff00"
+}
+CELL_NUMBER_COLORS= "#000000"
+CELL_NUMBER_FONTS={
+    2: ("Helvetica", 60, "bold"),
+    4: ("Helvetica", 60, "bold"),
+    8: ("Helvetica", 60, "bold"),
+    16:("Helvetica", 55, "bold"),
+    32:("Helvetica", 55, "bold"),
+    64:("Helvetica", 55, "bold"),
+    128:("Helvetica", 50, "bold"),
+    256:("Helvetica", 50, "bold"),
+    512:("Helvetica", 50, "bold"),
+    1024:("Helvetica", 45, "bold"),
+    2048:("Helvetica", 45, "bold")
+}
